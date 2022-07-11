04086f8bfbab64a7748abec15c0fefa01b2c99cf
M-Peeran <peeramakrabbi@gmail.com>
2022-07-11 21:32:04 +0530

    Added viewModel

--- app/src/main/java/com/peeranm/worldnews/feature_news/presentation/favourite_articles/FavouriteArticlesFragment.kt
+++ app/src/main/java/com/peeranm/worldnews/feature_news/presentation/favourite_articles/FavouriteArticlesFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.peeranm.worldnews.databinding.FragmentFavouriteArticlesBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,6 +15,8 @@ class FavouriteArticlesFragment : Fragment() {
     private var _binding: FragmentFavouriteArticlesBinding? = null
     private val binding: FragmentFavouriteArticlesBinding
     get() = _binding!!
+
+    private val viewModel: FavouriteArticlesViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
