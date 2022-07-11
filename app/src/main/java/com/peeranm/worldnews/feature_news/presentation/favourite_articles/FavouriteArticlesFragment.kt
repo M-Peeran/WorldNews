e9912e2270f9b2bac75a0ac35da9f64d55422d65
M-Peeran <peeramakrabbi@gmail.com>
2022-07-12 01:56:21 +0530

    Added logic to get saved articles, and to show each when clicked

--- app/src/main/java/com/peeranm/worldnews/feature_news/presentation/favourite_articles/FavouriteArticlesFragment.kt
+++ app/src/main/java/com/peeranm/worldnews/feature_news/presentation/favourite_articles/FavouriteArticlesFragment.kt
@@ -6,17 +6,26 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.peeranm.worldnews.core.collectWithLifecycle
 import com.peeranm.worldnews.databinding.FragmentFavouriteArticlesBinding
+import com.peeranm.worldnews.feature_news.data.local.entity.FavArticle
+import com.peeranm.worldnews.feature_news.utils.FavArticleAdapter
+import com.peeranm.worldnews.feature_news.utils.OnItemClickListener
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class FavouriteArticlesFragment : Fragment() {
+class FavouriteArticlesFragment : Fragment(), OnItemClickListener<FavArticle> {
 
     private var _binding: FragmentFavouriteArticlesBinding? = null
     private val binding: FragmentFavouriteArticlesBinding
     get() = _binding!!
 
     private val viewModel: FavouriteArticlesViewModel by viewModels()
+
+    private var adapter: FavArticleAdapter? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,11 +42,35 @@ class FavouriteArticlesFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.bindList()
 
+        collectWithLifecycle(viewModel.favArticles) {
+            adapter?.submitList(it)
+        }
+
+    }
+
+    private fun FragmentFavouriteArticlesBinding.bindList() {
+        adapter = FavArticleAdapter(this@FavouriteArticlesFragment)
+        val layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        listFavouriteArticles.adapter = adapter
+        listFavouriteArticles.layoutManager = layoutManager
+        listFavouriteArticles.addItemDecoration(DividerItemDecoration(requireContext(), layoutManager.orientation))
+    }
+
+    override fun onItemClick(view: View?, data: FavArticle, position: Int) {
+        findNavController().navigate(
+            FavouriteArticlesFragmentDirections.actionFavouriteArticlesFragmentToArticleFragment(
+                articleUrl = data.url,
+                isFavourite = true
+            )
+        )
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
+        adapter?.onClear()
+        adapter = null
         _binding = null
     }
 }
