315604a4af83c44bc879c1e000da3993ed9d2662
M-Peeran <peeramakrabbi@gmail.com>
2022-07-14 04:08:22 +0530

    Deleted unwanted action class

--- app/src/main/java/com/peeranm/worldnews/feature_news/presentation/article/UiAction.kt
+++
@@ -1,7 +0,0 @@
-package com.peeranm.worldnews.feature_news.presentation.article
-
-sealed class UiAction {
-    object None : UiAction()
-    object HideFabFavouriteArticle : UiAction()
-    class ShowMessage(val message: String) : UiAction()
-}
