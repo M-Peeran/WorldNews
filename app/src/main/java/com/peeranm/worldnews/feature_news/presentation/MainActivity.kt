b1cf41874f02768a7b31d39ae17bf979fc32e73d
M-Peeran <peeramakrabbi@gmail.com>
2022-07-14 18:53:48 +0530

    Added bottom nav bar

--- app/src/main/java/com/peeranm/worldnews/feature_news/presentation/MainActivity.kt
+++ app/src/main/java/com/peeranm/worldnews/feature_news/presentation/MainActivity.kt
@@ -2,13 +2,35 @@ package com.peeranm.worldnews.feature_news.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.peeranm.worldnews.R
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.peeranm.worldnews.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding: ActivityMainBinding
+    get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.bindBottomNavBar()
+    }
+
+    private fun ActivityMainBinding.bindBottomNavBar() {
+        bottomNavigationView.setupWithNavController(navHost.findNavController())
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
