b95825a758b9ecfa8463ef0cbd22f1b0410af3b0
M-Peeran <peeramakrabbi@gmail.com>
2022-07-16 01:37:10 +0530

    ResultListener is now set at onCreate

--- app/src/main/java/com/peeranm/worldnews/feature_news/presentation/news_feed/NewsFragment.kt
+++ app/src/main/java/com/peeranm/worldnews/feature_news/presentation/news_feed/NewsFragment.kt
@@ -9,10 +9,7 @@ import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.peeranm.worldnews.R
-import com.peeranm.worldnews.core.Constants
-import com.peeranm.worldnews.core.collectWithLifecycle
-import com.peeranm.worldnews.core.handleOnBackPressed
-import com.peeranm.worldnews.core.setActionbarTitle
+import com.peeranm.worldnews.core.*
 import com.peeranm.worldnews.databinding.FragmentNewsBinding
 import com.peeranm.worldnews.feature_news.model.Article
 import com.peeranm.worldnews.feature_news.presentation.news_feed.news_country_dialog.CountryDialog
@@ -45,10 +42,14 @@ class NewsFragment : Fragment(), OnItemClickListener<Article>, SearchView.OnQuer
         return binding.root
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setFragmentResultListener()
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setHasOptionsMenu(true)
-        setFragmentResultListener()
         handleOnBackPressed()
         setActionbarTitle(R.string.headlines)
         binding.bindList()
